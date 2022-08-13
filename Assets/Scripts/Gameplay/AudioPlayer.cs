8733dcac1ef973476b5a5f32c6fd4904c83d9f4f
DMITRIY GRIN <greendmitriy407@gmail.com>
2022-08-13 12:22:08 +0300

    added AudioPlayer

---
+++ Assets/Scripts/Gameplay/AudioPlayer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace Scripts.Gameplay
+{
+    public class AudioPlayer
+    {
+        [Inject]private AudioSource source;
+        [Inject(Id = "victory")] private AudioClip victory;
+        [Inject(Id = "lose")] private AudioClip lose;
+        [Inject(Id = "swap")] private AudioClip swap;
+        [Inject(Id = "match")] private AudioClip match;
+
+        public void PlayVictory()
+        {
+            source.PlayOneShot(victory);
+        }
+
+        public void PlayLose()
+        {
+            source.PlayOneShot(lose);
+        }
+
+        public void PlayCrystalSwap()
+        {
+            source.PlayOneShot(swap);
+        }
+
+        public void PlayCrystalMatch()
+        {
+            source.PlayOneShot(match);
+        }
+    }
+}
