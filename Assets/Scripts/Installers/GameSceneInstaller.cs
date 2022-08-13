48cac623c9e91799e3fe6d3b65a4187a63c29197
DMITRIY GRIN <greendmitriy407@gmail.com>
2022-08-13 12:22:51 +0300

    added GameSceneInstaller

---
+++ Assets/Scripts/Installers/GameSceneInstaller.cs
@@ -0,0 +1,51 @@
+﻿using Scripts.Gameplay;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+namespace Scripts.Installers
+{
+    public class GameSceneInstaller : MonoInstaller
+    {
+        [SerializeField] private AudioClip victory;
+        [SerializeField] private AudioClip lose;
+        [SerializeField] private AudioClip swap;
+        [SerializeField] private AudioClip match;
+        [SerializeField] private GameObject redCrystal;
+        [SerializeField] private GameObject greenCrystal;
+        [SerializeField] private GameObject blueCrystal;
+        [SerializeField] private GameObject pinkCrystal;
+        [SerializeField] private GameObject yellowCrystal;
+        private Dictionary<CrystalColor, GameObject> prefabs;
+
+
+        public override void InstallBindings()
+        {
+            BindAudio();
+            BindCrystals();
+        }
+
+        private void BindAudio()
+        {
+            Container.Bind<AudioSource>().FromComponentInHierarchy().AsSingle();
+            Container.Bind<AudioClip>().WithId("victory").FromInstance(victory);
+            Container.Bind<AudioClip>().WithId("lose").FromInstance(lose);
+            Container.Bind<AudioClip>().WithId("swap").FromInstance(swap);
+            Container.Bind<AudioClip>().WithId("match").FromInstance(match);
+            Container.Bind<AudioPlayer>().AsSingle();
+        }
+
+        private void BindCrystals()
+        {
+            prefabs = new Dictionary<CrystalColor, GameObject>();
+            prefabs.Add(CrystalColor.red, redCrystal);
+            prefabs.Add(CrystalColor.green, greenCrystal);
+            prefabs.Add(CrystalColor.blue, blueCrystal);
+            prefabs.Add(CrystalColor.pink, pinkCrystal);
+            prefabs.Add(CrystalColor.yellow, yellowCrystal);
+            Container.Bind<Dictionary<CrystalColor, GameObject>>().FromInstance(prefabs).WhenInjectedInto<CrystalFactory>();
+            Container.Bind<CrystalFactory>().AsSingle();
+            Container.Bind<IInputHandler>().To<MouseHandler>().AsSingle();
+        }
+    }
+}
